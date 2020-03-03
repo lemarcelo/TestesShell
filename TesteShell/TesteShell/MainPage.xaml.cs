ff239d41284688ba5997d823af4d1a8f83a193ce
Marcelo Pereira Santos <lemarcelo@outlook.com>
2020-03-02 22:46:46 -0300

    Add project files.

---
+++ TesteShell/TesteShell/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using TesteShell.Views;
+
+namespace TesteShell
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : Shell
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute("Page1", typeof(Page1));
+            Routing.RegisterRoute("Page2", typeof(Page2));
+            Routing.RegisterRoute("Page3", typeof(Page3));
+        }
+    }
+}
